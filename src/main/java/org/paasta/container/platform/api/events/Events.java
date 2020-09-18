3f60e08c1bb4ebd077b0c598851a2b409cf1252f
hrjin <hyunlee3jdh@gmail.com>
2020-09-18 19:07:40 +0900

    Events Logic Upload

---
+++ src/main/java/org/paasta/container/platform/api/events/Events.java
@@ -0,0 +1,46 @@
+package org.paasta.container.platform.api.events;
+
+import lombok.Data;
+import org.paasta.container.platform.api.common.CommonUtils;
+import org.paasta.container.platform.api.common.model.CommonMetaData;
+import org.paasta.container.platform.api.common.model.CommonObjectReference;
+
+/**
+ * Events Model 클래스
+ *
+ * @author hrjin
+ * @version 1.0
+ * @since 2020.09.17
+ */
+@Data
+public class Events {
+
+    private String resultCode;
+    private CommonMetaData metadata;
+    //private String action;
+    private int count;
+    //private String eventTime;
+    private String firstTimestamp;
+    private String lastTimestamp;
+    private String message;
+    //private String reason;
+    private EventSource source;
+    private String type;
+    private CommonObjectReference involvedObject;
+
+    public String getFirstTimestamp() {
+        return CommonUtils.procSetTimestamp(firstTimestamp);
+    }
+
+
+    public String getLastTimestamp() {
+        return CommonUtils.procSetTimestamp(lastTimestamp);
+    }
+
+
+    @Data
+    public class EventSource {
+        private String component;
+        private String host;
+    }
+}
---
+++ src/main/java/org/paasta/container/platform/api/events/EventsList.java
@@ -0,0 +1,20 @@
+package org.paasta.container.platform.api.events;
+
+import lombok.Data;
+
+import java.util.List;
+
+/**
+ * Events List Model 클래스
+ *
+ * @author hrjin
+ * @version 1.0
+ * @since 2020.09.17
+ */
+@Data
+public class EventsList {
+
+    private String resultCode;
+    private List<Events> items;
+
+}
