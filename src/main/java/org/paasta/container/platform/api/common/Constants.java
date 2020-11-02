7ff3f921d4267169b2c39de3c4c1d9ec40a52a39
hoon77 <slokang@naver.com>
2020-11-02 09:42:29 +0900

    Resource lIst paging process modify

--- src/main/java/org/paasta/container/platform/api/common/Constants.java
+++ src/main/java/org/paasta/container/platform/api/common/Constants.java
@@ -109,17 +109,6 @@ public class Constants {
     public static final String URI_LIMITRANGES_DETAIL = "/container-platform/{limitRangeName:.+}";
 
 
-    //login
-    public static final String LOGIN_SUCCESS = "Login Successful.";
-    public static final String LOGIN_FAIL = "Login Failed.";
-    public static final String NON_EXISTENT_ID = "존재하지 않는 사용자 아이디입니다.";
-    public static final String UNAVAILABLE_ID = "해당 사용자 아이디는 사용할 수 없습니다.";
-    public static final String INVALID_PASSWORD = "비밀번호가 올바르지 않습니다.";
-    public static final String ID_REQUIRED = "사용자 아이디를 입력해주세요.";
-    public static final String PASSWORD_REQUIRED = "비밀번호를 입력해주세요.";
-    public static final String ID_PASSWORD_REQUIRED = "사용자 아이디와 비밀번호를 입력해주세요.";
-
-
     /** 서비스 요청시 처리 메소드 kind 매핑 정보 */
     public static final String RESOURCE_SERVICEACCOUNT = "ServiceAccount";
     public static final String RESOURCE_ROLEBINDING = "RoleBinding";
@@ -149,6 +138,29 @@ public class Constants {
     public static final String RESOURCE_LIMITRANGE = "LimitRange";
     public static final String RESOURCE_ROLE = "Role";
 
+
+    //login
+    public static final String LOGIN_SUCCESS = "Login Successful.";
+    public static final String LOGIN_FAIL = "Login Failed.";
+    public static final String NON_EXISTENT_ID = "존재하지 않는 사용자 아이디입니다.";
+    public static final String UNAVAILABLE_ID = "해당 사용자 아이디는 사용할 수 없습니다.";
+    public static final String INVALID_PASSWORD = "비밀번호가 올바르지 않습니다.";
+    public static final String ID_REQUIRED = "사용자 아이디를 입력해주세요.";
+    public static final String PASSWORD_REQUIRED = "비밀번호를 입력해주세요.";
+    public static final String ID_PASSWORD_REQUIRED = "사용자 아이디와 비밀번호를 입력해주세요.";
+    public static final String INACTIVE_USER_ACCESS = "승인되지 않은 사용자입니다. 네임스페이스 지정이 필요합니다.";
+
+
+    public static final String LIMIT_ILLEGALARGUMENT= "limit(한 페이지에 가져올 리소스 최대 수) 는 반드시 0 이상이여아 합니다. limit >=0 ";
+    public static final String OFFSET_ILLEGALARGUMENT= "offset(목록 시작지점) 은 반드시 0 이상이여아 합니다. offset >=0 ";
+    public static final String OFFSET_REQUIRES_LIMIT_ILLEGALARGUMENT = "offset(목록 시작지점) 사용 시 limit(한 페이지에 가져올 리소스 최대 수) 값이 필요합니다.";
+
+    public static final String RESOURCE_NAME = "name";
+    public static final String RESOURCE_CREATIONTIMESTAMP = "creationTimestamp";
+    public static final String RESOURCE_METADATA = "metadata";
+
+    public static final String noName = "[-]";
+    public static final String separatorString =  "," ;
 
 
     /** 서비스 클래스의 Package */
