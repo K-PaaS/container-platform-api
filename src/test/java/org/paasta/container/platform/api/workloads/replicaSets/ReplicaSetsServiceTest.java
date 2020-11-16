711b63b7d027e2db0b5d58ed8841ac7bd20336c6
hrjin <hyunlee3jdh@gmail.com>
2020-11-16 21:19:41 +0900

    Add ReplicaSets Service Test Case

--- src/test/java/org/paasta/container/platform/api/workloads/replicaSets/ReplicaSetsServiceTest.java
+++ src/test/java/org/paasta/container/platform/api/workloads/replicaSets/ReplicaSetsServiceTest.java
@@ -1,54 +1,60 @@
 package org.paasta.container.platform.api.workloads.replicaSets;
 
-import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.paasta.container.platform.api.common.CommonService;
-import org.paasta.container.platform.api.common.PropertyService;
-import org.paasta.container.platform.api.common.RestTemplateService;
+import org.paasta.container.platform.api.common.*;
 import org.paasta.container.platform.api.common.model.CommonResourcesYaml;
 import org.paasta.container.platform.api.common.model.ResultStatus;
-import org.paasta.container.platform.api.workloads.pods.Pods;
-import org.paasta.container.platform.api.workloads.pods.PodsAdmin;
-import org.paasta.container.platform.api.workloads.pods.PodsList;
-import org.paasta.container.platform.api.workloads.pods.PodsListAdmin;
+import org.springframework.http.HttpMethod;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.when;
 
 @RunWith(SpringRunner.class)
 @TestPropertySource("classpath:application.yml")
 public class ReplicaSetsServiceTest {
     private static final String CLUSTER = "cp-cluster";
     private static final String NAMESPACE = "cp-namespace";
-    private static final String SERVICE_NAME = "cp-service-name";
+    private static final String REPLICASETS_NAME = "cp-service-name";
     private static final String YAML_STRING = "test-yaml-string";
 
+    private static final String SELECTOR = "test-selector";
+    private static final String FIELD_SELECTOR = "?fieldSelector=metadata.namespace!=kubernetes-dashboard,metadata.namespace!=kube-node-lease,metadata.namespace!=kube-public,metadata.namespace!=kube-system,metadata.namespace!=temp-namespace";
 
+    private static final int OFFSET = 0;
+    private static final int LIMIT = 0;
+    private static final String ORDER_BY = "creationTime";
+    private static final String ORDER = "desc";
+    private static final String SEARCH_NAME = "";
+    private static final boolean isAdmin = true;
+    private static final boolean isNotAdmin = false;
 
     private static HashMap gResultMap = null;
 
-    private static PodsList gResultListModel = null;
-    private static PodsList gFinalResultListModel = null;
+    private static ReplicaSetsList gResultListModel = null;
+    private static ReplicaSetsList gFinalResultListModel = null;
 
-    private static PodsListAdmin gResultListAdminModel = null;
-    private static PodsListAdmin gFinalResultListAdminModel = null;
+    private static ReplicaSetsListAdmin gResultListAdminModel = null;
+    private static ReplicaSetsListAdmin gFinalResultListAdminModel = null;
 
-    private static Pods gResultModel = null;
-    private static Pods gFinalResultModel = null;
+    private static ReplicaSets gResultModel = null;
+    private static ReplicaSets gFinalResultModel = null;
 
-    private static PodsAdmin gResultAdminModel = null;
-    private static PodsAdmin gFinalResultAdminModel = null;
+    private static ReplicaSetsAdmin gResultAdminModel = null;
+    private static ReplicaSetsAdmin gFinalResultAdminModel = null;
 
     private static CommonResourcesYaml gResultYamlModel = null;
     private static CommonResourcesYaml gFinalResultYamlModel = null;
 
     private static ResultStatus gResultStatusModel = null;
-    private static ResultStatus gResultFailModel = null;
     private static ResultStatus gFinalResultStatusModel = null;
 
     @Mock
@@ -67,10 +73,30 @@ public class ReplicaSetsServiceTest {
     public void setUp() {
         gResultMap = new HashMap();
 
-    }
+        gResultListModel = new ReplicaSetsList();
+        gFinalResultListModel = new ReplicaSetsList();
+        gFinalResultListModel.setResultCode(Constants.RESULT_STATUS_SUCCESS);
 
-    @After
-    public void tearDown() {
+        gResultListAdminModel = new ReplicaSetsListAdmin();
+        gFinalResultListAdminModel = new ReplicaSetsListAdmin();
+        gFinalResultListAdminModel.setResultCode(Constants.RESULT_STATUS_SUCCESS);
+
+        gResultModel = new ReplicaSets();
+        gFinalResultModel = new ReplicaSets();
+        gFinalResultModel.setResultCode(Constants.RESULT_STATUS_SUCCESS);
+
+        gResultAdminModel = new ReplicaSetsAdmin();
+        gFinalResultAdminModel = new ReplicaSetsAdmin();
+        gFinalResultAdminModel.setResultCode(Constants.RESULT_STATUS_SUCCESS);
+
+        gResultYamlModel = new CommonResourcesYaml();
+        gFinalResultYamlModel = new CommonResourcesYaml();
+        gFinalResultYamlModel.setResultCode(Constants.RESULT_STATUS_SUCCESS);
+        gFinalResultYamlModel.setSourceTypeYaml(YAML_STRING);
+
+        gResultStatusModel = new ResultStatus();
+        gFinalResultStatusModel = new ResultStatus();
+        gFinalResultStatusModel.setResultCode(Constants.RESULT_STATUS_SUCCESS);
     }
 
     /**
@@ -79,11 +105,17 @@ public class ReplicaSetsServiceTest {
     @Test
     public void getReplicaSetsList_Valid_ReturnModel() {
         // given
+        when(propertyService.getCpMasterApiListReplicaSetsListUrl()).thenReturn("/apis/apps/v1/namespaces/{namespace}/replicasets");
+        when(restTemplateService.send(Constants.TARGET_CP_MASTER_API, "/apis/apps/v1/namespaces/" +NAMESPACE+ "/replicasets", HttpMethod.GET, null, Map.class)).thenReturn(gResultMap);
+        when(commonService.setResultObject(gResultMap, ReplicaSetsList.class)).thenReturn(gResultListModel);
+        when(commonService.resourceListProcessing(gResultListModel, OFFSET, LIMIT, ORDER_BY, ORDER, SEARCH_NAME, ReplicaSetsList.class)).thenReturn(gResultListModel);
+        when(commonService.setResultModel(gResultListModel, Constants.RESULT_STATUS_SUCCESS)).thenReturn(gFinalResultListModel);
 
         // when
+        ReplicaSetsList resultList = replicaSetsService.getReplicaSetsList(NAMESPACE, OFFSET, LIMIT, ORDER_BY, ORDER, SEARCH_NAME);
 
         // then
-
+        assertEquals(Constants.RESULT_STATUS_SUCCESS, resultList.getResultCode());
     }
 
     /**
@@ -92,11 +124,16 @@ public class ReplicaSetsServiceTest {
     @Test
     public void getReplicaSets_Valid_ReturnModel() {
         // given
+        when(propertyService.getCpMasterApiListReplicaSetsGetUrl()).thenReturn("/apis/apps/v1/namespaces/{namespace}/replicasets/{name}");
+        when(restTemplateService.send(Constants.TARGET_CP_MASTER_API, "/apis/apps/v1/namespaces/" + NAMESPACE + "/replicasets/" + REPLICASETS_NAME, HttpMethod.GET, null, Map.class)).thenReturn(gResultMap);
+        when(commonService.setResultObject(gResultMap, ReplicaSets.class)).thenReturn(gResultModel);
+        when(commonService.setResultModel(gResultModel, Constants.RESULT_STATUS_SUCCESS)).thenReturn(gFinalResultModel);
 
         // when
+        ReplicaSets result = replicaSetsService.getReplicaSets(NAMESPACE, REPLICASETS_NAME);
 
         // then
-
+        assertEquals(Constants.RESULT_STATUS_SUCCESS, result.getResultCode());
     }
 
     /**
@@ -106,11 +143,16 @@ public class ReplicaSetsServiceTest {
     @Test
     public void getReplicaSetsAdmin_Valid_ReturnModel() {
         // given
+        when(propertyService.getCpMasterApiListReplicaSetsGetUrl()).thenReturn("/apis/apps/v1/namespaces/{namespace}/replicasets/{name}");
+        when(restTemplateService.sendAdmin(Constants.TARGET_CP_MASTER_API, "/apis/apps/v1/namespaces/" + NAMESPACE + "/replicasets/" + REPLICASETS_NAME, HttpMethod.GET, null, Map.class)).thenReturn(gResultMap);
+        when(commonService.setResultObject(gResultMap, ReplicaSetsAdmin.class)).thenReturn(gResultAdminModel);
+        when(commonService.setResultModel(gResultAdminModel, Constants.RESULT_STATUS_SUCCESS)).thenReturn(gFinalResultAdminModel);
 
         // when
+        ReplicaSetsAdmin result = (ReplicaSetsAdmin) replicaSetsService.getReplicaSetsAdmin(NAMESPACE, REPLICASETS_NAME);
 
         // then
-
+        assertEquals(Constants.RESULT_STATUS_SUCCESS, result.getResultCode());
     }
 
     /**
@@ -119,11 +161,17 @@ public class ReplicaSetsServiceTest {
     @Test
     public void getReplicaSetsYaml_Valid_ReturnModel() {
         // given
+        when(propertyService.getCpMasterApiListReplicaSetsGetUrl()).thenReturn("/apis/apps/v1/namespaces/{namespace}/replicasets/{name}");
+        when(restTemplateService.send(Constants.TARGET_CP_MASTER_API, "/apis/apps/v1/namespaces/" + NAMESPACE + "/replicasets/" + REPLICASETS_NAME, HttpMethod.GET, null, String.class, Constants.ACCEPT_TYPE_YAML)).thenReturn(YAML_STRING);
+        when(commonService.setResultObject(gResultMap, CommonResourcesYaml.class)).thenReturn(gResultYamlModel);
+        when(commonService.setResultModel(gResultYamlModel, Constants.RESULT_STATUS_SUCCESS)).thenReturn(gFinalResultYamlModel);
 
         // when
+        CommonResourcesYaml result = (CommonResourcesYaml) replicaSetsService.getReplicaSetsYaml(NAMESPACE, REPLICASETS_NAME, gResultMap);
 
         // then
-
+        assertEquals(YAML_STRING, result.getSourceTypeYaml());
+        assertEquals(Constants.RESULT_STATUS_SUCCESS, result.getResultCode());
     }
 
     /**
@@ -132,11 +180,17 @@ public class ReplicaSetsServiceTest {
     @Test
     public void getReplicaSetsAdminYaml_Valid_ReturnModel() {
         // given
+        when(propertyService.getCpMasterApiListReplicaSetsGetUrl()).thenReturn("/apis/apps/v1/namespaces/{namespace}/replicasets/{name}");
+        when(restTemplateService.sendAdmin(Constants.TARGET_CP_MASTER_API, "/apis/apps/v1/namespaces/" + NAMESPACE + "/replicasets/" + REPLICASETS_NAME, HttpMethod.GET, null, String.class, Constants.ACCEPT_TYPE_YAML)).thenReturn(YAML_STRING);
+        when(commonService.setResultObject(gResultMap, CommonResourcesYaml.class)).thenReturn(gResultYamlModel);
+        when(commonService.setResultModel(gResultYamlModel, Constants.RESULT_STATUS_SUCCESS)).thenReturn(gFinalResultYamlModel);
 
         // when
+        CommonResourcesYaml result = (CommonResourcesYaml) replicaSetsService.getReplicaSetsAdminYaml(NAMESPACE, REPLICASETS_NAME, gResultMap);
 
         // then
-
+        assertEquals(YAML_STRING, result.getSourceTypeYaml());
+        assertEquals(Constants.RESULT_STATUS_SUCCESS, result.getResultCode());
     }
 
     /**
@@ -145,11 +199,17 @@ public class ReplicaSetsServiceTest {
     @Test
     public void getReplicaSetsListLabelSelector_Valid_ReturnModel() {
         // given
+        when(propertyService.getCpMasterApiListReplicaSetsListUrl()).thenReturn("/apis/apps/v1/namespaces/{namespace}/replicasets");
+        when(restTemplateService.send(Constants.TARGET_CP_MASTER_API, "/apis/apps/v1/namespaces/" + NAMESPACE + "/replicasets?labelSelector=" + SELECTOR, HttpMethod.GET, null, Map.class)).thenReturn(gResultMap);
+        when(commonService.setResultObject(gResultMap, ReplicaSetsList.class)).thenReturn(gResultListModel);
+        when(commonService.setCommonItemMetaDataBySelector(gResultListModel, ReplicaSetsList.class)).thenReturn(gResultListModel);
+        when(commonService.setResultModel(gResultListModel, Constants.RESULT_STATUS_SUCCESS)).thenReturn(gFinalResultListModel);
 
         // when
+        ReplicaSetsList resultList = replicaSetsService.getReplicaSetsListLabelSelector(NAMESPACE, SELECTOR);
 
         // then
-
+        assertEquals(Constants.RESULT_STATUS_SUCCESS, resultList.getResultCode());
     }
 
     /**
@@ -158,11 +218,17 @@ public class ReplicaSetsServiceTest {
     @Test
     public void getReplicaSetsListLabelSelectorAdmin_Valid_ReturnModel() {
         // given
+        when(propertyService.getCpMasterApiListReplicaSetsListUrl()).thenReturn("/apis/apps/v1/namespaces/{namespace}/replicasets");
+        when(restTemplateService.sendAdmin(Constants.TARGET_CP_MASTER_API, "/apis/apps/v1/namespaces/" + NAMESPACE + "/replicasets?labelSelector=" + SELECTOR, HttpMethod.GET, null, Map.class)).thenReturn(gResultMap);
+        when(commonService.setResultObject(gResultMap, ReplicaSetsListAdmin.class)).thenReturn(gResultListAdminModel);
+        when(commonService.setCommonItemMetaDataBySelector(gResultListAdminModel, ReplicaSetsListAdmin.class)).thenReturn(gResultListAdminModel);
+        when(commonService.setResultModel(gResultListAdminModel, Constants.RESULT_STATUS_SUCCESS)).thenReturn(gFinalResultListAdminModel);
 
         // when
+        ReplicaSetsListAdmin resultList = replicaSetsService.getReplicaSetsListLabelSelectorAdmin(NAMESPACE, SELECTOR);
 
         // then
-
+        assertEquals(Constants.RESULT_STATUS_SUCCESS, resultList.getResultCode());
     }
 
     /**
@@ -171,11 +237,17 @@ public class ReplicaSetsServiceTest {
     @Test
     public void getReplicaSetsListAdmin_Valid_ReturnModel() {
         // given
+        when(propertyService.getCpMasterApiListReplicaSetsListUrl()).thenReturn("/apis/apps/v1/namespaces/{namespace}/replicasets");
+        when(restTemplateService.sendAdmin(Constants.TARGET_CP_MASTER_API, "/apis/apps/v1/namespaces/" + NAMESPACE + "/replicasets", HttpMethod.GET, null, Map.class)).thenReturn(gResultMap);
+        when(commonService.setResultObject(gResultMap, ReplicaSetsListAdmin.class)).thenReturn(gResultListAdminModel);
+        when(commonService.resourceListProcessing(gResultListAdminModel, OFFSET, LIMIT, ORDER_BY, ORDER, SEARCH_NAME, ReplicaSetsListAdmin.class)).thenReturn(gResultListAdminModel);
+        when(commonService.setResultModel(gResultListAdminModel, Constants.RESULT_STATUS_SUCCESS)).thenReturn(gFinalResultListAdminModel);
 
         // when
+        ReplicaSetsListAdmin resultList = (ReplicaSetsListAdmin) replicaSetsService.getReplicaSetsListAdmin(NAMESPACE, OFFSET, LIMIT, ORDER_BY, ORDER, SEARCH_NAME);
 
         // then
-
+        assertEquals(Constants.RESULT_STATUS_SUCCESS, resultList.getResultCode());
     }
 
     /**
@@ -184,24 +256,52 @@ public class ReplicaSetsServiceTest {
     @Test
     public void createReplicaSets_Valid_ReturnModel() {
         // given
+        when(propertyService.getCpMasterApiListReplicaSetsCreateUrl()).thenReturn("/apis/apps/v1/namespaces/{namespace}/replicasets");
+        when(restTemplateService.sendYaml(Constants.TARGET_CP_MASTER_API, "/apis/apps/v1/namespaces/" + NAMESPACE + "/replicasets", HttpMethod.POST, YAML_STRING, Object.class, isAdmin)).thenReturn(gResultStatusModel);
+        when(commonService.setResultObject(gResultStatusModel, ResultStatus.class)).thenReturn(gResultStatusModel);
+        when(commonService.setResultModelWithNextUrl(gResultStatusModel, Constants.RESULT_STATUS_SUCCESS, Constants.URI_WORKLOAD_REPLICA_SETS)).thenReturn(gFinalResultStatusModel);
 
         // when
+        ResultStatus result = (ResultStatus) replicaSetsService.createReplicaSets(NAMESPACE, YAML_STRING, isAdmin);
 
         // then
-
+        assertEquals(gFinalResultStatusModel, result);
     }
 
     /**
-     * ReplicaSets 삭제(Delete ReplicaSets) Test
+     * ReplicaSets 삭제(Delete ReplicaSets) Admin Test
      */
     @Test
-    public void deleteReplicaSets_Valid_ReturnModel() {
+    public void deleteReplicaSets_Admin_Valid_ReturnModel() {
         // given
+        when(propertyService.getCpMasterApiListReplicaSetsDeleteUrl()).thenReturn("/apis/apps/v1/namespaces/{namespace}/replicasets/{name}");
+        when(restTemplateService.sendAdmin(Constants.TARGET_CP_MASTER_API, "/apis/apps/v1/namespaces/" + NAMESPACE + "/replicasets/" + REPLICASETS_NAME, HttpMethod.DELETE, null, ResultStatus.class)).thenReturn(gResultStatusModel);
+        when(commonService.setResultObject(gResultStatusModel, ResultStatus.class)).thenReturn(gResultStatusModel);
+        when(commonService.setResultModelWithNextUrl(gResultStatusModel, Constants.RESULT_STATUS_SUCCESS, Constants.URI_WORKLOAD_REPLICA_SETS)).thenReturn(gFinalResultStatusModel);
 
         // when
+        ResultStatus result = replicaSetsService.deleteReplicaSets(NAMESPACE, REPLICASETS_NAME, isAdmin);
 
         // then
+        assertEquals(gFinalResultStatusModel, result);
+    }
 
+    /**
+     * ReplicaSets 삭제(Delete ReplicaSets) Not Admin Test
+     */
+    @Test
+    public void deleteReplicaSets_Not_Admin_Valid_ReturnModel() {
+        // given
+        when(propertyService.getCpMasterApiListReplicaSetsDeleteUrl()).thenReturn("/apis/apps/v1/namespaces/{namespace}/replicasets/{name}");
+        when(restTemplateService.send(Constants.TARGET_CP_MASTER_API, "/apis/apps/v1/namespaces/" + NAMESPACE + "/replicasets/" + REPLICASETS_NAME, HttpMethod.DELETE, null, ResultStatus.class)).thenReturn(gResultStatusModel);
+        when(commonService.setResultObject(gResultStatusModel, ResultStatus.class)).thenReturn(gResultStatusModel);
+        when(commonService.setResultModelWithNextUrl(gResultStatusModel, Constants.RESULT_STATUS_SUCCESS, Constants.URI_WORKLOAD_REPLICA_SETS)).thenReturn(gFinalResultStatusModel);
+
+        // when
+        ResultStatus result = replicaSetsService.deleteReplicaSets(NAMESPACE, REPLICASETS_NAME, isNotAdmin);
+
+        // then
+        assertEquals(gFinalResultStatusModel, result);
     }
 
     /**
@@ -210,11 +310,16 @@ public class ReplicaSetsServiceTest {
     @Test
     public void updateReplicaSets_Valid_ReturnModel() {
         // given
+        when(propertyService.getCpMasterApiListReplicaSetsUpdateUrl()).thenReturn("/apis/apps/v1/namespaces/{namespace}/replicasets/{name}");
+        when(restTemplateService.sendYaml(Constants.TARGET_CP_MASTER_API, "/apis/apps/v1/namespaces/" + NAMESPACE + "/replicasets/" + REPLICASETS_NAME, HttpMethod.PUT, YAML_STRING, ResultStatus.class, isAdmin)).thenReturn(gResultStatusModel);
+        when(commonService.setResultObject(gResultStatusModel, ResultStatus.class)).thenReturn(gResultStatusModel);
+        when(commonService.setResultModelWithNextUrl(gResultStatusModel, Constants.RESULT_STATUS_SUCCESS, Constants.URI_WORKLOAD_REPLICA_SETS_DETAIL.replace("{replicaSetName:.+}", REPLICASETS_NAME))).thenReturn(gFinalResultStatusModel);
 
         // when
+        ResultStatus result = replicaSetsService.updateReplicaSets(NAMESPACE, REPLICASETS_NAME, YAML_STRING, isAdmin);
 
         // then
-
+        assertEquals(gFinalResultStatusModel, result);
     }
 
     /**
@@ -223,10 +328,17 @@ public class ReplicaSetsServiceTest {
     @Test
     public void getReplicaSetsListAllNamespacesAdmin_Valid_ReturnModel() {
         // given
+        when(propertyService.getCpMasterApiListReplicaSetsListAllNamespacesUrl()).thenReturn("/apis/apps/v1/replicasets");
+        when(commonService.generateFieldSelectorForExceptNamespace(Constants.RESOURCE_NAMESPACE)).thenReturn(FIELD_SELECTOR);
+        when(restTemplateService.sendAdmin(Constants.TARGET_CP_MASTER_API, "/apis/apps/v1/replicasets" + FIELD_SELECTOR, HttpMethod.GET, null, Map.class)).thenReturn(gResultMap);
+        when(commonService.setResultObject(gResultMap, ReplicaSetsListAdmin.class)).thenReturn(gResultListAdminModel);
+        when(commonService.resourceListProcessing(gResultListAdminModel, OFFSET, LIMIT, ORDER_BY, ORDER, SEARCH_NAME, ReplicaSetsListAdmin.class)).thenReturn(gResultListAdminModel);
+        when(commonService.setResultModel(gResultListAdminModel, Constants.RESULT_STATUS_SUCCESS)).thenReturn(gFinalResultListAdminModel);
 
         // when
+        ReplicaSetsListAdmin resultList =  (ReplicaSetsListAdmin) replicaSetsService.getReplicaSetsListAllNamespacesAdmin(OFFSET, LIMIT, ORDER_BY, ORDER, SEARCH_NAME);
 
         // then
-
+        assertEquals(gFinalResultListAdminModel, resultList);
     }
 }
