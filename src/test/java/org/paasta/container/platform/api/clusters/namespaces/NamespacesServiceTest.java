2d67140d103fb88a2a05910307b9ff3d6899e3da
hoon77 <slokang@naver.com>
2020-11-16 21:43:26 +0900

    Add namespacesService Test

--- src/test/java/org/paasta/container/platform/api/clusters/namespaces/NamespacesServiceTest.java
+++ src/test/java/org/paasta/container/platform/api/clusters/namespaces/NamespacesServiceTest.java
@@ -5,19 +5,77 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.paasta.container.platform.api.clusters.resourceQuotas.ResourceQuotasList;
 import org.paasta.container.platform.api.common.CommonService;
+import org.paasta.container.platform.api.common.Constants;
 import org.paasta.container.platform.api.common.PropertyService;
 import org.paasta.container.platform.api.common.RestTemplateService;
-import org.paasta.container.platform.api.workloads.deployments.DeploymentsService;
+import org.paasta.container.platform.api.common.model.CommonResourcesYaml;
+import org.paasta.container.platform.api.common.model.CommonStatusCode;
+import org.paasta.container.platform.api.common.model.ResultStatus;
+import org.paasta.container.platform.api.users.UsersService;
+import org.springframework.http.HttpMethod;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.when;
 
 @RunWith(SpringRunner.class)
 @TestPropertySource("classpath:application.yml")
 public class NamespacesServiceTest {
 
+    private static final String CLUSTER = "cp-cluster";
+    private static final String NAMESPACE = "test-namespace";
+    private static final String USER_ID = "test-user";
+    private static final String YAML_STRING = "test-yaml-string";
+    private static final String FIELD_SELECTOR = "?fieldSelector=metadata.name!=kubernetes-dashboard,metadata.name!=kube-node-lease,metadata.name!=kube-public,metadata.name!=kube-system,metadata.name!=temp-namespace";
+
+    private static final int OFFSET = 0;
+    private static final int LIMIT = 0;
+    private static final String ORDER_BY = "creationTime";
+    private static final String ORDER = "desc";
+    private static final String SEARCH_NAME = "";
+    private static final boolean isAdmin = true;
+    private static final boolean isNotAdmin = false;
+    public static final String USERS = "users";
+    private static HashMap gResultMap = null;
+    private static HashMap gResultAdminMap = null;
+    private static HashMap gResultAdminFailMap = null;
+
+
+    private static List<String> gResultList;
+    private static Namespaces gResultModel = null;
+    private static Namespaces gFinalResultModel = null;
+
+    private static NamespacesList gResultListModel = null;
+    private static NamespacesList gFinalResultListModel = null;
+    private static NamespacesList gFinalResultListFailModel = null;
+
+
+    private static NamespacesAdmin gResultAdminModel = null;
+    private static NamespacesAdmin gFinalResultAdminModel = null;
+    private static NamespacesAdmin gFinalResultAdminFailModel = null;
+
+    private static NamespacesListAdmin gResultListAdminModel = null;
+    private static NamespacesListAdmin gFinalResultListAdminModel = null;
+    private static NamespacesListAdmin gFinalResultListAdminFailModel = null;
+
+
+    private static CommonResourcesYaml gResultYamlModel = null;
+    private static CommonResourcesYaml gFinalResultYamlModel = null;
+
+    private static ResultStatus gResultStatusModel = null;
+    private static ResultStatus gResultFailModel = null;
+    private static ResultStatus gFinalResultStatusModel = null;
+
+
+    private static ResourceQuotasList gResourceQuotasListModel = null;
     @Mock
     RestTemplateService restTemplateService;
 
@@ -27,36 +85,203 @@ public class NamespacesServiceTest {
     @Mock
     PropertyService propertyService;
 
+    @Mock
+    UsersService usersService;
+
+
     @InjectMocks
     NamespacesService namespacesService;
 
     @Before
     public void setUp() throws Exception {
+        gResultMap = new HashMap();
+
+
+        gResultStatusModel = new ResultStatus();
+        gResultStatusModel.setResultCode(Constants.RESULT_STATUS_SUCCESS);
+        gResultStatusModel.setResultMessage(Constants.RESULT_STATUS_SUCCESS);
+        gResultStatusModel.setHttpStatusCode(CommonStatusCode.OK.getCode());
+        gResultStatusModel.setDetailMessage(CommonStatusCode.OK.getMsg());
+
+        gResultStatusModel = new ResultStatus();
+        gResultStatusModel.setResultCode(Constants.RESULT_STATUS_SUCCESS);
+        gResultStatusModel.setResultMessage(Constants.RESULT_STATUS_SUCCESS);
+        gResultStatusModel.setHttpStatusCode(CommonStatusCode.OK.getCode());
+        gResultStatusModel.setDetailMessage(CommonStatusCode.OK.getMsg());
+
+        gFinalResultStatusModel = new ResultStatus();
+        gFinalResultStatusModel.setResultCode(Constants.RESULT_STATUS_SUCCESS);
+        gFinalResultStatusModel.setResultMessage(Constants.RESULT_STATUS_SUCCESS);
+        gFinalResultStatusModel.setHttpStatusCode(CommonStatusCode.OK.getCode());
+        gFinalResultStatusModel.setDetailMessage(CommonStatusCode.OK.getMsg());
+        gFinalResultStatusModel.setNextActionUrl(Constants.URI_WORKLOAD_DEPLOYMENTS);
+
+        // 리스트가져옴
+        gResultListModel = new NamespacesList();
+
+        gFinalResultListModel = new NamespacesList();
+        gFinalResultListModel.setResultCode(Constants.RESULT_STATUS_SUCCESS);
+
+        gFinalResultListFailModel = new NamespacesList();
+        gFinalResultListFailModel.setResultCode(Constants.RESULT_STATUS_FAIL);
+
+        // 하나만 가져옴
+        gResultModel = new Namespaces();
+        gFinalResultModel = new Namespaces();
+        gFinalResultModel.setResultCode(Constants.RESULT_STATUS_SUCCESS);
+
+        gResultFailModel = new ResultStatus();
+        gResultFailModel.setResultCode(Constants.RESULT_STATUS_FAIL);
+        gResultFailModel.setResultMessage(Constants.RESULT_STATUS_FAIL);
+        gResultFailModel.setHttpStatusCode(CommonStatusCode.NOT_FOUND.getCode());
+        gResultFailModel.setDetailMessage(CommonStatusCode.NOT_FOUND.getMsg());
+
+        gResultYamlModel = new CommonResourcesYaml();
+        gFinalResultYamlModel = new CommonResourcesYaml();
+        gFinalResultYamlModel.setResultCode(Constants.RESULT_STATUS_SUCCESS);
+        gFinalResultYamlModel.setResultMessage(Constants.RESULT_STATUS_SUCCESS);
+        gFinalResultYamlModel.setHttpStatusCode(CommonStatusCode.OK.getCode());
+        gFinalResultYamlModel.setDetailMessage(CommonStatusCode.OK.getMsg());
+        gFinalResultYamlModel.setSourceTypeYaml(YAML_STRING);
+
+        // 리스트가져옴
+        gResultAdminMap = new HashMap();
+        gResultListAdminModel = new NamespacesListAdmin();
+        gFinalResultListAdminModel = new NamespacesListAdmin();
+
+        gFinalResultListAdminModel = new NamespacesListAdmin();
+        gFinalResultListAdminModel.setResultCode(Constants.RESULT_STATUS_SUCCESS);
+        gFinalResultListAdminModel.setResultMessage(Constants.RESULT_STATUS_SUCCESS);
+        gFinalResultListAdminModel.setHttpStatusCode(CommonStatusCode.OK.getCode());
+        gFinalResultListAdminModel.setDetailMessage(CommonStatusCode.OK.getMsg());
+
+        gFinalResultListAdminFailModel = new NamespacesListAdmin();
+        gFinalResultListAdminFailModel.setResultCode(Constants.RESULT_STATUS_FAIL);
+        gFinalResultListAdminFailModel.setResultMessage(Constants.RESULT_STATUS_FAIL);
+        gFinalResultListAdminFailModel.setHttpStatusCode(CommonStatusCode.NOT_FOUND.getCode());
+        gFinalResultListAdminFailModel.setDetailMessage(CommonStatusCode.NOT_FOUND.getMsg());
+
+
+        // 하나만 가져옴
+        gResultAdminModel = new NamespacesAdmin();
+        gFinalResultAdminModel = new NamespacesAdmin();
+        gFinalResultAdminModel.setResultCode(Constants.RESULT_STATUS_SUCCESS);
+        gFinalResultAdminModel.setResultMessage(Constants.RESULT_STATUS_SUCCESS);
+        gFinalResultAdminModel.setHttpStatusCode(CommonStatusCode.OK.getCode());
+        gFinalResultAdminModel.setDetailMessage(CommonStatusCode.OK.getMsg());
+
+        gFinalResultAdminFailModel = new NamespacesAdmin();
+        gFinalResultAdminFailModel.setResultCode(Constants.RESULT_STATUS_FAIL);
+        gFinalResultAdminFailModel.setResultMessage(Constants.RESULT_STATUS_FAIL);
+        gFinalResultAdminFailModel.setHttpStatusCode(CommonStatusCode.NOT_FOUND.getCode());
+        gFinalResultAdminFailModel.setDetailMessage(CommonStatusCode.NOT_FOUND.getMsg());
     }
 
     @Test
-    public void getNamespaces() {
+    public void getNamespaces_Valid_ReturnModel() {
+
+        //when
+        when(propertyService.getCpMasterApiListNamespacesGetUrl()).thenReturn("/api/v1/namespaces/{namespace}");
+        when(restTemplateService.send(Constants.TARGET_CP_MASTER_API, "/api/v1/namespaces/" + NAMESPACE, HttpMethod.GET, null, Map.class)).thenReturn(gResultMap);
+        when(commonService.setResultObject(gResultMap, Namespaces.class)).thenReturn(gResultModel);
+        when(commonService.setResultModel(gResultModel, Constants.RESULT_STATUS_SUCCESS)).thenReturn(gFinalResultModel);
+
+        //call method
+        Namespaces result = namespacesService.getNamespaces(NAMESPACE);
+
+        //compare result
+        assertThat(result).isNotNull();
+        assertEquals(Constants.RESULT_STATUS_SUCCESS, result.getResultCode());
+
     }
 
     @Test
-    public void getNamespacesAdmin() {
+    public void getNamespacesAdmin_Valid_ReturnModel() {
+
+        //when
+        when(propertyService.getCpMasterApiListNamespacesGetUrl()).thenReturn("/api/v1/namespaces/{namespace}");
+        when(restTemplateService.sendAdmin(Constants.TARGET_CP_MASTER_API, "/api/v1/namespaces/" + NAMESPACE, HttpMethod.GET, null, Map.class)).thenReturn(gResultMap);
+        when(commonService.setResultObject(gResultMap, NamespacesAdmin.class)).thenReturn(gResultAdminModel);
+        when(commonService.setResultModel(gResultAdminModel, Constants.RESULT_STATUS_SUCCESS)).thenReturn(gFinalResultAdminModel);
+
+        //call method
+        NamespacesAdmin result = (NamespacesAdmin) namespacesService.getNamespacesAdmin(NAMESPACE);
+
+        //when
+        //compare result
+        assertThat(result).isNotNull();
+        assertEquals(Constants.RESULT_STATUS_SUCCESS, result.getResultCode());
+
+    }
+
+
+    @Test
+    public void getNamespacesListAdmin_Valid_ReturnModel() {
+
+        //when
+        when(propertyService.getCpMasterApiListNamespacesListUrl()).thenReturn("/api/v1/namespaces");
+        when(restTemplateService.sendAdmin(Constants.TARGET_CP_MASTER_API, "/api/v1/namespaces"+
+                commonService.generateFieldSelectorForExceptNamespace(Constants.RESOURCE_CLUSTER), HttpMethod.GET, null, Map.class)).thenReturn(gResultAdminMap);
+
+        when(commonService.setResultObject(gResultAdminMap, NamespacesListAdmin.class)).thenReturn(gResultListAdminModel);
+        when(commonService.resourceListProcessing(gResultListAdminModel, OFFSET, LIMIT, ORDER_BY, ORDER, SEARCH_NAME, NamespacesListAdmin.class)).thenReturn(gResultListAdminModel);
+        when(commonService.setResultModel(gResultListAdminModel, Constants.RESULT_STATUS_SUCCESS)).thenReturn(gFinalResultListAdminModel);
+
+        //call method
+        NamespacesListAdmin resultList = (NamespacesListAdmin) namespacesService.getNamespacesListAdmin(OFFSET, LIMIT, ORDER_BY, ORDER, SEARCH_NAME);
+
+        //compare result
+        assertThat(resultList).isNotNull();
+        assertEquals(Constants.RESULT_STATUS_SUCCESS, resultList.getResultCode());
+
     }
 
     @Test
-    public void getNamespacesList() {
+    public void getNamespacesAdminYaml_Valid_ReturnModel() {
+
+        //when
+        when(propertyService.getCpMasterApiListNamespacesGetUrl()).thenReturn("/api/v1/namespaces/{namespace}");
+        when(restTemplateService.sendAdmin(Constants.TARGET_CP_MASTER_API, "/api/v1/namespaces/{namespace}"
+                + NAMESPACE , HttpMethod.GET, null, String.class, Constants.ACCEPT_TYPE_YAML)).thenReturn(YAML_STRING);
+
+        when(commonService.setResultObject(gResultMap, CommonResourcesYaml.class)).thenReturn(gResultYamlModel);
+        when(commonService.setResultModel(gResultYamlModel, Constants.RESULT_STATUS_SUCCESS)).thenReturn(gFinalResultYamlModel);
+
+        //call method
+        CommonResourcesYaml result = (CommonResourcesYaml) namespacesService.getNamespacesAdminYaml(NAMESPACE, gResultMap);
+
+        //compare result
+        assertEquals(YAML_STRING, result.getSourceTypeYaml());
+        assertEquals(Constants.RESULT_STATUS_SUCCESS, result.getResultCode());
     }
 
-    @Test
-    public void getNamespacesListAdmin() {
-    }
-
-    @Test
-    public void getNamespacesYaml() {
-    }
 
     @Test
     public void deleteNamespaces() {
+
+
+        when(propertyService.getCpMasterApiListNamespacesDeleteUrl()).thenReturn("/api/v1/namespaces/{name}");
+        when(restTemplateService.sendAdmin(Constants.TARGET_CP_MASTER_API, "/api/v1/namespaces/" + NAMESPACE,  HttpMethod.DELETE, null, ResultStatus.class)).thenReturn(gResultStatusModel);
+
+     /*   when(usersService.getUsersNameListByNamespace(NAMESPACE).get(USERS)).thenReturn(gResultList);
+        when(usersService.deleteUsers(usersService.getUsers(NAMESPACE, USER_ID))).thenReturn(gResultStatusModel);
+
+        when(commonService.setResultObject(gResultStatusModel, ResultStatus.class)).thenReturn(gResultStatusModel);
+        when(commonService.setResultModelWithNextUrl(gResultStatusModel, Constants.RESULT_STATUS_SUCCESS, Constants.URI_CLUSTER_NAMESPACES)).thenReturn(gFinalResultStatusModel);
+
+        ResultStatus result = namespacesService.deleteNamespaces(NAMESPACE);
+        assertEquals(gFinalResultStatusModel, result);*/
     }
+
+
+    @Test
+    public void modifyResourceQuotas_Valid_ReturnModel() {
+        when(propertyService.getCpMasterApiListResourceQuotasListUrl()).thenReturn("/api/v1/namespaces/{namespace}/resourcequotas");
+        when(restTemplateService.sendAdmin(Constants.TARGET_CP_MASTER_API, "/api/v1/namespaces/" + NAMESPACE + "/resourcequotas", HttpMethod.GET, null, ResourceQuotasList.class)).thenReturn(gResourceQuotasListModel);
+
+    }
+
+
 
     @Test
     public void createInitNamespaces() {
@@ -67,6 +292,10 @@ public class NamespacesServiceTest {
     }
 
     @Test
-    public void getNamespacesListForSelectbox() {
+    public void getNamespacesListForSelectbox_Valid_ReturnModel() {
     }
+
+
+
+
 }
